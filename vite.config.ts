1cc7501afe2b559020f9ceb35025256a0953efdc
shari <sohaib2good@gmail.com>
2026-09-08 18:14:55 +0500

    fix(vite): ignore data folder in file watcher and initialize clean registrations store

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,10 @@ export default defineConfig(() => {
         '@': path.resolve(__dirname, 'src'),
       },
     },
+    server: {
+      watch: {
+        ignored: ['**/data/**'],
+      },
+    },
   };
 });
